d0a0e7c8aecf285b66213662171a08c5147e9389
ArlinoMagalhaes <145712366+ArlinoMagalhaes@users.noreply.github.com>
2024-06-17 10:16:24 -0300

    Create Listagem 3.2.js

---
+++ Listagem 3.2.js
@@ -0,0 +1,8 @@
+{
+ "first" : "Arlino"
+ "lastename" : "Magalhães"
+ "email" : ["home": "arlinoh@gmail.com",
+ "work": "arlino@ufpi.edu.br"] ,
+ "phone" : ["home": "+810001000"] ,
+ "adress" : []
+ }
